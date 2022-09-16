1297d1b109c88be9cc13715bd285d2fd7996f7b8
Mako05001 <112701714+Mako05001@users.noreply.github.com>
2022-09-16 16:47:01 +0200

    Update Program.cs

--- Uppdrag 2.1/Program.cs
+++ Uppdrag 2.1/Program.cs
@@ -17,7 +17,7 @@ namespace uppdrag2._13
             int ptot = p1 + p2 + p3 / 3;
             int ptotal = ptot / 3;
             Console.Write("Medel lönen är ");
-            Console.WriteLine(ptot);
+            Console.WriteLine(ptotal);
         }
     }
 }
